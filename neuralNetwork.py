a4fc77bc207bcbdd15146369b3a21675d94a38e5
Dazz1e <2468691031@qq.com>
2022-03-14 17:05:50 +0800

    .

---
+++ neuralNetwork.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class NeuralNetwork:
+    def __init__(self, layerNumber, neuronNumberPerLayer, learningRate):
+        self.layerNumber = layerNumber
+        self.neuronNumberPerLayer = neuronNumberPerLayer
+        self.learningRate = learningRate
+        self.weight = []
+
+    # initialize weights
+
+    # set activation function
+
+    # forward propagation
+
+    # back propagation
+
+    # calculate error
+
+
