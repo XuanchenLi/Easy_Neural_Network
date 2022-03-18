ae99402f47b983d4e1c09eb07d950e0ab9e86b46
Dazz1e <2468691031@qq.com>
2022-03-18 18:06:50 +0800

    basic

--- main.py
+++ main.py
@@ -1,2 +1,80 @@
-import neuralNetwork
+from neuralNetwork import NeuralNetwork
+from sklearn.datasets import load_iris
+import random
+import os
+import struct
+import numpy as np
+
+PATH = './'
+
+
+def get_data(dataset, kind, seed=0):
+    if dataset == "MNIST":
+        labels_p = os.path.join(PATH, 'MNIST', '%s-labels.idx1-ubyte'%kind)
+        images_p = os.path.join(PATH, 'MNIST', '%s-images.idx3-ubyte'%kind)
+        with open(labels_p, 'rb') as lbpath:
+            magic, n = struct.unpack('>II', lbpath.read(8))
+            labels = np.fromfile(lbpath, dtype=np.uint8)
+
+        ls = []
+        for i in range(labels.size):
+            temp = np.zeros(10)
+            temp[labels[i]] = 1
+            ls.append(temp.T)
+        labels = np.array(ls)
+
+        with open(images_p, 'rb') as imgpath:
+            magic, n, rows, cols = struct.unpack(">IIII", imgpath.read(16))
+            images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+
+            images = images / 2550
+        return images, labels
+    elif dataset == "Iris":
+        iris = load_iris()
+        N, _ = iris.data.shape
+        index = [i for i in range(len(iris.data))]
+        random.seed(seed)
+        random.shuffle(index)
+        attributes = np.array(iris.data[index])
+        target = np.array(iris.target[index])
+        if kind == "train":
+            attributes = attributes[:int(N*0.7)]
+            target = target[:int(N*0.7)]
+        elif kind == "valid":
+            attributes = attributes[int(N * 0.7 + 1):]
+            target = target[int(N * 0.7 + 1):]
+        else:
+            raise ValueError("dataset not exist")
+        ls = []
+        for i in range(target.size):
+            temp = np.zeros(3)
+            temp[target[i]] = 1
+            ls.append(temp.T)
+        target = np.array(ls)
+        return attributes, target
+    else:
+        raise ValueError("dataset not exist")
+
+
+if __name__ == '__main__':
+    train_x, train_y = get_data("Iris", "train", 100)
+    valid_x, valid_y = get_data("Iris", "valid", 100)
+    node_num = [4, 30, 3]
+    network = NeuralNetwork(2, node_num, 0.3, 50)
+    network.set_activate("sigmoid")
+    network.gaussian_weights()
+    network.fit(train_x, train_y)
+    acc = network.test(valid_x, valid_y)
+    print(acc)
+    '''
+    train_x, train_y = get_data("MNIST", "train")
+    valid_x, valid_y = get_data("MNIST", "t10k")
+    node_num = [784, 30, 10]
+    network = NeuralNetwork(2, node_num, 0.3, 5)
+    network.set_activate("sigmoid")
+    network.gaussian_weights()
+    network.fit(train_x, train_y)
+    acc = network.test(valid_x, valid_y)
+    print(acc)
+    '''
 
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,30 +2,96 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, layerNumber, neuronNumberPerLayer, learningRate):
+    def __init__(self, layerNumber, neuronNumberPerLayer, learningRate, epochs):
+        self.outputs = []
+        self.errors = []
         self.layerNumber = layerNumber
         self.neuronNumberPerLayer = neuronNumberPerLayer
         self.learningRate = learningRate
         self.weight = []
+        self.activation_function = None
+        self.epochs = epochs
 
     # initialize weights
-    def gaussian_weights(self, loc, scale):
+    def gaussian_weights(self):
         for i in range(self.layerNumber):
             self.weight.append(np.random.normal(
-                loc, scale,
+                0.0, pow(self.neuronNumberPerLayer[i+1], -0.5),
                 (self.neuronNumberPerLayer[i+1], self.neuronNumberPerLayer[i])
             ))
 
     def zero_weights(self):
         for i in range(self.layerNumber):
-            self.weight.append(np.zeros(self.neuronNumberPerLayer[i+1]*self.neuronNumberPerLayer[i]))
+            self.weight.append(np.zeros(self.neuronNumberPerLayer[i+1] * self.neuronNumberPerLayer[i]))
 
     # set activation function
+    def set_activate(self, function):
+        if function == "sigmoid":
+            self.activation_function = lambda x: 1.0/(1.0+np.exp(-x))
+        else:
+            raise ValueError("not implemented")
 
     # forward propagation
+    def forward(self, inputs, labels):
+        inputs_t = np.array(inputs, ndmin=2).T
+        labels_t = np.array(labels, ndmin=2).T
+        self.outputs = []
+        self.outputs.append(inputs_t)
+        self.errors = []
+        for i in range(self.layerNumber):
+            tmp_inputs = np.dot(self.weight[i], inputs_t)
+            tmp_outputs = self.activation_function(tmp_inputs)
+            inputs_t = tmp_outputs
+            self.outputs.append(tmp_outputs)
+        for i in range(self.layerNumber):
+            if i == 0:
+                self.errors.append(labels_t - self.outputs[-1])
+            else:  # 计算右层残差加权求和
+                self.errors.append(
+                    np.dot((self.weight[self.layerNumber-i]).T, self.errors[i-1])
+                )
 
     # back propagation
+    def back_propagation(self):
+        for i in range(self.layerNumber):
+            # 更新权重
+            self.weight[self.layerNumber-i-1] += self.learningRate * \
+                                                 np.dot(
+                                                     (self.errors[i] *
+                                                      self.outputs[-1-i] * (1.0 - self.outputs[-1-i])),
+                                                     np.transpose(self.outputs[-1-i-1])
+                                                 )
+
+    # fit
+    def fit(self, inputs, targets):
+        for i in range(self.epochs):
+            for k in range(len(inputs)):
+                self.forward(inputs[k], targets[k])
+                self.back_propagation()
+
+    # predict
+    def predict(self, inputs):
+        inputs_t = np.array(inputs, ndmin=2).T
+        for i in range(self.layerNumber):
+            tmp_inputs = np.dot(self.weight[i], inputs_t)
+            tmp_outputs = self.activation_function(tmp_inputs)
+            inputs_t = tmp_outputs
+        return inputs_t
+
+    # test
+    def test(self, valid_x, valid_y):
+
+        cnt = 0
+        for i in range(len(valid_x)):
+            pre = self.predict(valid_x[i])
+            print(list(pre).index(max(list(pre))), valid_y[i])
+            if list(pre).index(max(list(pre))) == list(valid_y[i]).index(1):
+                cnt += 1
+        return cnt/len(valid_x)
+
+
+
+    # set loss function
 
     # calculate error
-
 
